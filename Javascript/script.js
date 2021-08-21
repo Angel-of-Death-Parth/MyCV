55e89e5ab56f9425771da41b3ffc62b1b8284958
Angel_of_Death <88188657+Angel-of-Death-Parth@users.noreply.github.com>
2021-08-21 22:34:18 +0530

    finally

--- Javascript/script.js
+++ Javascript/script.js
@@ -27,3 +27,8 @@ function navShow() {
         button_nav.innerHTML = '&#x2716;'
     }
 }
+$('section').mousemove(function(e){
+    var moveX = (e.pageX*-1/15);
+    var moveY = (e.pageY*-1/15);
+    $(this).css('background-position', moveX + 'px ' + moveY + 'px')
+})
